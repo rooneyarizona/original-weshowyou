8bb184899ec23f808b8c5dfc9cf0a7c0a390055f
rooneyarizona <sagaralastair@gmail.com>
2024-02-14 16:19:02 -0700

    fixed video submit

--- dist/index.js
+++ dist/index.js
@@ -31,5 +31,5 @@ function addClickNavigation(items, page) {
 
 // Navigate to other pages when clicked
 addClickNavigation(searchItems, 'search.html');
-addClickNavigation(uploadItems, 'videosubmit.html');
+addClickNavigation(uploadItems, 'submitvideo.html');
 addClickNavigation(menuItems, 'account.html');
