e25af6c754b462994de88a0319303e98f368a9b2
rooneyarizona <sagaralastair@gmail.com>
2024-03-05 15:34:15 -0700

    updated server.js

--- dist/server.js
+++ dist/server.js
@@ -1,3 +1,45 @@
+const express = require('express');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+const config = require('./config');
+
+const app = express();
+const port = 3000;
+
+// Make credentials secure
+const db = mysql.createConnection(config.db);
+
+db.connect((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log('Connected to database');
+});
+
+// Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// API Endpoint to handle form submission
+app.post('/submit-form', (req, res) => {
+  const { firstName, lastName, userName, password, dateOfBirth, dateJoined, eMailAddress } = req.body;
+  const sql = 'INSERT INTO users (firstName, lastName, userName, password, dateOfBirth, dateJoined, eMailAddress) VALUES (?, ?, ?, ?, ?, ?, ?)';
+  const values = [firstName, lastName, userName, password, dateOfBirth, dateJoined, eMailAddress];
+  
+  db.query(sql, values, (err, result) => {
+    if (err) {
+      res.status(500).send('Error inserting data');
+      throw err;
+    }
+    res.status(200).send('Data inserted successfully');
+  });
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
 $(document).ready(function(){
     $('#form-test').submit(function(e){
         e.preventDefault();
